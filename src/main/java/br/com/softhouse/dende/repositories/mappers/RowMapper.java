19ba3deb51c970303d131f2742c24ced61d57be5
Deivid <deividsouza252@gmail.com>
2026-05-17 22:50:50 -0300

    refatoração 1.5

--- src/main/java/br/com/softhouse/dende/repositories/mappers/RowMapper.java
+++
@@ -1,7 +0,0 @@
-package br.com.softhouse.dende.repositories.mappers;
-//package br.com.dende.softhouse.repositorry;
-
-@FunctionalInterface
-public interface RowMapper<T> {
-    T mapRow(String[] row);
-}
